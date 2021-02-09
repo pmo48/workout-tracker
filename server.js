77001cc49f40a59bb68e31c7cf5916b12c255ddb
pmo48 <pmoriarty100@gmail.com>
2021-02-08 21:54:22 -0800

    added comments

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
+//requiring express for server handling, Mongoose for Mongo DB and logger for logging mongo requests
+
 const express = require("express");
 const mongoose = require("mongoose");
 const logger = require('morgan');
 
+//requires in data model
+
 const Fitness = require("./models/fitness.js");
 
+//either uses the environment port where deployed or localhost3000
 
 const PORT = process.env.PORT || 3000;
 
@@ -16,6 +21,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
+//logic for connecting mongo in heroku
 
 mongoose.connect(
   
@@ -25,6 +31,8 @@ mongoose.connect(
   useCreateIndex: true,
   useFindAndModify: false
 });
+
+//html and api files for routes
 
 app.use(require("./routes/html-routes.js"));
 app.use(require("./routes/api-routes.js"));
