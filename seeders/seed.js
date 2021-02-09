935b721153c62f10ca166e6cfa6d40665c623f2e
pmo48 <pmoriarty100@gmail.com>
2021-02-08 17:15:23 -0800

    updated seeders file

--- seeders/seed.js
+++ seeders/seed.js
@@ -124,8 +124,8 @@ let workoutSeed = [
   }
 ];
 
-Fitness.Workout.deleteMany({})
-  .then(() => db.Workout.collection.insertMany(workoutSeed))
+Fitness.deleteMany({})
+  .then(() => Fitness.collection.insertMany(workoutSeed))
   .then(data => {
     console.log(data.result.n + " records inserted!");
     process.exit(0);
